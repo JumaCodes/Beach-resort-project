71aa032f971028e449990e617bc5b5deeec9e47f
CodexLegacy <73387073+CodexLegacy@users.noreply.github.com>
2021-10-06 16:11:46 +0100

    A full functional Beach_resort Project with filtering functions

---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Banner from '../components/Banner'
+import Hero from '../components/Hero'
+import { Link } from 'react-router-dom'
+import Services from '../components/Services'
+import FeaturedRooms from '../components/FeatureRooms';
+
+
+export default function Home() {
+  return (
+    <>
+    <Hero>
+      <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $199">
+        <Link to="/rooms" className="btn-primary">
+          our rooms
+        </Link>
+      </Banner>
+      </Hero>
+      <Services />
+      <FeaturedRooms />
+      </>
+  )
+}
+
+
